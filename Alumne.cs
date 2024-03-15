42e900e1a18be2e3ba7b75ad1417d7a7bbafc12b
Hugo Marta Doñágueda <huma280@vidalibarraquer.net>
2024-03-15 09:01:59 +0100

    Version 0

---
+++ Alumne.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDataBinding1
+{
+    internal class Alumne
+    {
+        public string Nom { get; set; }
+        public string Cognoms { get; set; }
+        public string Email { get; set; }
+        public DateTime Data_naixement {  get; set; }
+        private bool _Horari_mati;
+        private bool _Horari_tarda;
+        public bool Curset1 { get; set; }
+        public bool Curset2 { get; set; }
+        public bool Curset3 { get; set; }
+
+        public bool Horari_mati
+        {
+            get => _Horari_mati;
+            set
+            {
+                _Horari_mati = value;
+                _Horari_tarda = !value;
+            }
+        }
+        public bool Horari_tarda
+        {
+            get => _Horari_tarda;
+            set
+            {
+                _Horari_mati = !value;
+                _Horari_tarda = value;
+            }
+        }
+
+        public Alumne(string nom, string cognoms, string email, DateTime dataNaixement, bool horariMati, bool curset1, bool curset2, bool curset3)
+        {
+            Nom = nom;
+            Cognoms = cognoms;
+            Email = email;
+            Data_naixement = dataNaixement;
+            Horari_mati = horariMati;
+            Horari_tarda = !horariMati;
+            Curset1 = curset1;
+            Curset2 = curset2;
+            Curset3 = curset3;
+        }
+
+    }
+}
